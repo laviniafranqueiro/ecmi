33e5b23a3f50b97828efd8579103ca82c4ab65e1
laviniafranqueiro <133999323+laviniafranqueiro@users.noreply.github.com>
2023-05-31 16:50:03 -0300

    Update atividade2.py

--- atividade2.py
+++ atividade2.py
@@ -1,8 +1,10 @@
 import streamlit as st
 import pandas as pd 
-import csv
-st.title('trabalho csv')
+st.title('tarefa do Josir')
 st.caption('Lavínia Franqueiro')
 
-df = pd.read_csv('acidentes.csv', sep =';')
+df = pd.read_csv('acidentes.csv', sep =',')
 st.dataframe(df)
+
+chart_data = df[['Road Accidents during 2018', 'Road Accidents during 2019', 'Road Accidents during 2020']]
+st.line_chart(chart_data)
